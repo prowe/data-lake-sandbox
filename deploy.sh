81daeabb54dad6748728f4adf265af9b92d7912e
Paul Rowe <prowe@sourceallies.com>
2020-03-11 20:40:18 -0500

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+aws cloudformation package \
+    --template-file=cloudformation.template.yml \
+    --s3-bucket=prowe-sai-sandbox-dev-deploy \
+    --output-template-file=cloudformation.transformed.yml
+
+aws cloudformation deploy \
+    --stack-name=prowe-serverless-lake \
+    --template-file=cloudformation.transformed.yml
---
+++ login.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pushd .
+cd ~/Development/sai-aws-auth/cli-auth
+source login-aws-sandbox-dev.sh
+
+popd
